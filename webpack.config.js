ec77753c4821e783701c403579bef058a82e85c5
Artem Ostretsov <OstretsovAA@gmail.com>
2018-05-07 14:41:59 +0300

    ~ configs

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,5 @@ module.exports = {
                 }
             }
         ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-    ],
+    }
 };
